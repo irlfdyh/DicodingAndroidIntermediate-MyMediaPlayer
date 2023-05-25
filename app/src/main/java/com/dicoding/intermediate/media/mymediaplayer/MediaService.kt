3c828a4ff979da91503531b8be94df55eb3ef1a6
Irshal Firdeansyah <irshalfir@gmail.com>
2023-05-25 18:59:56 +0700

    update MediaService to handle incoming message

--- app/src/main/java/com/dicoding/intermediate/media/mymediaplayer/MediaService.kt
+++ app/src/main/java/com/dicoding/intermediate/media/mymediaplayer/MediaService.kt
@@ -4,8 +4,14 @@ import android.app.Service
 import android.content.Intent
 import android.media.AudioAttributes
 import android.media.MediaPlayer
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
+import android.os.Message
+import android.os.Messenger
+import android.util.Log
 import java.io.IOException
+import java.lang.ref.WeakReference
 
 class MediaService : Service(), MediaPlayerCallback {
 
@@ -13,7 +19,34 @@ class MediaService : Service(), MediaPlayerCallback {
     private var mediaPlayer: MediaPlayer? = null
 
     override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+        Log.d(TAG, "onBind: ")
+        return messenger.binder
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val action = intent?.action
+
+        if (action != null) {
+            when (action) {
+                ACTION_CREATE -> {
+                    if (mediaPlayer == null) {
+                        init()
+                    }
+                }
+                ACTION_DESTROY -> {
+                    if (mediaPlayer?.isPlaying as Boolean) {
+                        stopSelf()
+                    }
+                }
+                else -> {
+                    init()
+                }
+            }
+        }
+
+        Log.d(TAG, "onStartCommand: ")
+
+        return flags
     }
 
     override fun onPlay() {
@@ -57,7 +90,27 @@ class MediaService : Service(), MediaPlayerCallback {
         }
 
         mediaPlayer?.setOnErrorListener { _, _, _ -> false }
+    }
 
+    private val messenger = Messenger(IncomingHandler(this))
+
+    internal class IncomingHandler(playerCallback: MediaPlayerCallback) : Handler(Looper.getMainLooper()) {
+        private val mediaPlayerCallbackWeakReference: WeakReference<MediaPlayerCallback> = WeakReference(playerCallback)
+        override fun handleMessage(msg: Message) {
+            when (msg.what) {
+                PLAY -> mediaPlayerCallbackWeakReference.get()?.onPlay()
+                STOP -> mediaPlayerCallbackWeakReference.get()?.onStop()
+                else -> super.handleMessage(msg)
+            }
+        }
+    }
+
+    companion object {
+        const val ACTION_CREATE = "com.dicoding.intermediate.media.mymediaplayer.create"
+        const val ACTION_DESTROY = "com.dicoding.intermediate.media.mymediaplayer.destroy"
+        const val TAG = "MediaService"
+        const val PLAY = 1
+        const val STOP = 0
     }
 
 }

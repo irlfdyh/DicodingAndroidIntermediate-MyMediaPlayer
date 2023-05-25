8c9eeed49405c743cf644b2833066a69d0e6e114
Irshal Firdeansyah <irshalfir@gmail.com>
2023-05-25 19:10:48 +0700

    implement MediaService on MainActivity

--- app/src/main/java/com/dicoding/intermediate/media/mymediaplayer/MainActivity.kt
+++ app/src/main/java/com/dicoding/intermediate/media/mymediaplayer/MainActivity.kt
@@ -1,10 +1,35 @@
 package com.dicoding.intermediate.media.mymediaplayer
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.*
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "MainActivity"
+    }
+
+    private var serviceMessenger: Messenger? = null
+    private lateinit var boundServiceIntent: Intent
+    private var serviceBound = false
+
+    private val serviceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            serviceMessenger = Messenger(service)
+            serviceBound = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            serviceMessenger = null
+            serviceBound = false
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,12 +39,39 @@ class MainActivity : AppCompatActivity() {
         val btnStop = findViewById<Button>(R.id.btn_stop)
 
         btnPlay.setOnClickListener {
-
+            if (serviceBound) {
+                try {
+                    serviceMessenger?.send(Message.obtain(null, MediaService.PLAY, 0, 0))
+                } catch (e: RemoteException) {
+                    e.printStackTrace()
+                }
+            }
         }
 
         btnStop.setOnClickListener {
-
+            if (serviceBound) {
+                try {
+                    serviceMessenger?.send(Message.obtain(null, MediaService.STOP, 0, 0))
+                } catch (e: RemoteException) {
+                    e.printStackTrace()
+                }
+            }
         }
+
+        boundServiceIntent = Intent(this@MainActivity, MediaService::class.java)
+        boundServiceIntent.action = MediaService.ACTION_CREATE
+
+        startService(boundServiceIntent)
+        bindService(boundServiceIntent, serviceConnection, Context.BIND_AUTO_CREATE)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
+        unbindService(serviceConnection)
+        boundServiceIntent.action = MediaService.ACTION_DESTROY
+
+        startService(boundServiceIntent)
     }
 
 }
